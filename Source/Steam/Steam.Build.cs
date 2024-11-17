53812de4efabfb41dfb5f3bce8fab039bc79a193
Risensy <58880572+Risensy@users.noreply.github.com>
2024-11-17 13:21:13 +0300

    Updated to 5.5 engine version

--- Source/Steam/Steam.Build.cs
+++ Source/Steam/Steam.Build.cs
@@ -11,14 +11,15 @@ public class Steam : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
-        DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
 }
